022a9b9ee6e694b89ef27fddefbdf413c944c920
waelabdallah <wael.abdallah@carnet.tn>
2024-07-26 04:16:16 +0100

    first commit

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const favicon = require('serve-favicon');
 const path = require('path');
 const utils = require('./utils');
+const { textToSpeech } = require('./azure-cognitiveservices-speech');
 
 // fn to create express server
 const create = async () => {
@@ -21,6 +22,30 @@ const create = async () => {
 
     // root route - serve static file
     app.get('/', (req, res) => res.sendFile(path.join(__dirname, '../public/client.html')));
+
+    // creates a temp file on server, the streams to client
+/* eslint-disable no-unused-vars */
+app.get('/text-to-speech', async (req, res, next) => {
+    
+    const { key, region, phrase, file } = req.query;
+    
+    if (!key || !region || !phrase) res.status(404).send('Invalid query string');
+    
+    let fileName = null;
+    
+    // stream from file or memory
+    if (file && file === true) {
+        // eslint-disable-next-line no-undef
+        fileName = `./temp/stream-from-file-${timeStamp()}.mp3`;
+    }
+    
+    const audioStream = await textToSpeech(key, region, phrase, fileName);
+    res.set({
+        'Content-Type': 'audio/mpeg',
+        'Transfer-Encoding': 'chunked'
+    });
+    audioStream.pipe(res);
+});
 
     // Catch errors
     app.use(utils.logErrors);
